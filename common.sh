78ba844b026dc154dbea16e75d7eac48498aaa26
puritys <puritys.chen@gmail.com>
2018-01-28 09:36:23 +0000

    update docker common script

--- common.sh
+++ common.sh
@@ -10,7 +10,12 @@ getDefaultVolume() {
     volume="-v /www:/www"
 }
 
+docker_my_init() {
+    imageName=`echo $imageName | tr '[:upper:]' '[:lower:]'`
+}
+
 build() {
+    docker_my_init
     if [ "x1" == "x$enableNodeDockerfileInclude" ]; then
         dockerfile-include  -i $dockerfile -o Dockerfile
     fi
@@ -18,17 +23,20 @@ build() {
 }
 
 stop() {
+    docker_my_init
     docker stop $containerName || true
     docker rm -f $containerName || true
 }
 
 start() {
+    docker_my_init
     stop
     docker run -d -t $port $volume --name $containerName $imageName /bin/bash
     docker exec -d $containerName bash -c "sh /root/start.sh"
 }
 
 root() {
+    docker_my_init
     stop
     docker run -t -i $port --name $containerName $volume  $imageName  /bin/bash
 }
