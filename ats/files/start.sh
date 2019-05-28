1602ae9638d481ab3066a2726cd5016329e6a09e
puritys <puritys.chen@gmail.com>
2019-05-28 08:52:16 +0000

    fix permission problem

--- ats/files/start.sh
+++ ats/files/start.sh
@@ -6,6 +6,8 @@ bash /root/logging.sh > /usr/local/etc/trafficserver/logging.config
 
 ln -sf /usr/local/var/log/trafficserver/squid.log /tmp/log
 
+/usr/local/bin/traffic_server -Cclear
+rm /usr/local/var/trafficserver/server.lock
 /usr/local/bin/traffic_manager
 
 #exec "$@";
