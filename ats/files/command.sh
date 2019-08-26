a7a48c3bb9de62d07c8030a77538c9c1e7c493f6
puritys <puritys.chen@gmail.com>
2019-08-26 08:49:20 +0000

    To support docker logs

--- ats/files/command.sh
+++
@@ -1,6 +0,0 @@
-command=$1
-
-if [ "xlog" == "x$command" ]; then
-    file=`ls -c /usr/local/var/log/trafficserver/ | grep squid | tail -n 1`
-    tail -f $file
-fi
--- ats/files/start.sh
+++ ats/files/start.sh
@@ -4,10 +4,13 @@ bash /root/records.sh > /usr/local/etc/trafficserver/records.config
 bash /root/ssl_multicert.sh > /usr/local/etc/trafficserver/ssl_multicert.config
 bash /root/logging.sh > /usr/local/etc/trafficserver/logging.config
 
-ln -sf /usr/local/var/log/trafficserver/squid.log /tmp/log
-
 /usr/local/bin/traffic_server -Cclear
 rm /usr/local/var/trafficserver/server.lock
-/usr/local/bin/traffic_manager
+/usr/local/bin/traffic_manager start &
 
-#exec "$@";
+while true
+do
+    tail -f /usr/local/var/log/trafficserver/squid.log
+    sleep 1 
+done
+
