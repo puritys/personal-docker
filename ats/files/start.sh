6740ef6f79162cedf0fdafc50254e0ec6dd94f99
puritys <puritys.chen@gmail.com>
2020-05-16 10:32:38 +0000

    allow delete method

--- ats/files/start.sh
+++ ats/files/start.sh
@@ -3,6 +3,7 @@ bash /root/remap.sh > /usr/local/etc/trafficserver/remap.config
 bash /root/records.sh > /usr/local/etc/trafficserver/records.config
 bash /root/ssl_multicert.sh > /usr/local/etc/trafficserver/ssl_multicert.config
 bash /root/logging.sh > /usr/local/etc/trafficserver/logging.config
+cp /root/ip_allow.config  /usr/local/etc/trafficserver/
 
 /usr/local/bin/traffic_server -Cclear
 rm -f /usr/local/var/trafficserver/server.lock
